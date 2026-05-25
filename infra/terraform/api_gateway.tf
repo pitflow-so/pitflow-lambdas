87457a79668600402f030741cddaf9f1439a707b
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-05-25 16:55:39 -0300

    choce changed S{local.eks_alb_url}/proxy to local.eks_alb_url

--- infra/terraform/api_gateway.tf
+++ infra/terraform/api_gateway.tf
@@ -40,7 +40,7 @@ resource "aws_apigatewayv2_route" "route_budget_post" {
 resource "aws_apigatewayv2_integration" "eks_backend" {
   api_id             = aws_apigatewayv2_api.pitflow_api.id
   integration_type   = "HTTP_PROXY"
-  integration_uri    = "${local.eks_alb_url}/{proxy}"
+  integration_uri    = local.eks_alb_url
   integration_method = "ANY"
 
   request_parameters = {
