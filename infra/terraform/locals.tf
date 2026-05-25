63546daaafcc5706fed2493bc7a44cd4eb31b334
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-05-25 13:42:00 -0300

    chore: rename alb_url name

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
   # Decodifica o secret e extrai apenas a URL
-  eks_alb_url = jsondecode(data.aws_secretsmanager_secret_version.pitflow.secret_string)["API_PUBLIC_URL"]
+  eks_alb_url = jsondecode(data.aws_secretsmanager_secret_version.pitflow.secret_string)["LB_URL"]
 }
