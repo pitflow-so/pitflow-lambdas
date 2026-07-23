356b07dc2f26190bc5fc62ffc13d3759ccb7bb0d
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-07-22 23:29:35 -0300

    chore: changed s3 to tfstate

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-backend-fiap-pitflow"
+    bucket = "tfstate-backend-pitflow-bootstrap"
     key    = "infra/terraform/lambdas/terraform.tfstate"
     region = "us-east-1"
   }
